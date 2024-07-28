413f5045f6696091085f5cd077cf854a5bac76fa
Al Bluemle <abluemleiv@gmail.com>
2024-07-28 14:21:37 -0400

    Performs Reordering

--- includes/nft.py
+++ includes/nft.py
@@ -30,21 +30,50 @@ class Nft:
 
     def CreateImage(self):
         subdirectory = ''
+        need_reorder = False
+        need_reorder_eyes = False
         if 'man' in self.nft_type:
             subdirectory = 'Man/'
+            need_reorder = self.Check_Reorder()
+            need_reorder_eyes = self.Reorder_Eyes_Man()
         elif 'female' in self.nft_type:
             subdirectory = 'Female/'
-    
+            need_reorder_eyes = self.Reorder_Eyes_Female()
+
+        if need_reorder:
+            layer_to_move = self.layers.pop(7)
+            dna_to_move = self.dnaPaths.pop(7)
+            self.layers.insert(0, layer_to_move)
+            self.dnaPaths.insert(0, dna_to_move)
+
+        if need_reorder_eyes:
+            layer_to_move = self.layers.pop(7)
+            dna_to_move = self.dnaPaths.pop(7)
+            self.layers.insert(6, layer_to_move)
+            self.dnaPaths.insert(6, dna_to_move)
+            
         baseLayer = Image.open(os.path.dirname(__file__) + '/../input/assets/' + subdirectory + self.layers[0] + '/' + self.dnaPaths[0])
         baseLayer = baseLayer.convert('RGBA')
 
-        for i in range(1, len(self.layers)):                        
+        for i in range(1, len(self.layers)):              
             frontLayer = Image.open(os.path.dirname(__file__) + '/../input/assets/' + subdirectory + self.layers[i] + '/' + self.dnaPaths[i])
             frontLayer = frontLayer.convert('RGBA')
             baseLayer = Image.alpha_composite(baseLayer, frontLayer)
 
         baseLayer = baseLayer.resize((1600,1600),Image.ANTIALIAS)
         baseLayer.save(os.path.dirname(__file__) + '/../output/nfts/' + self.folder_path + '/' + str(self.number) + '.png', quality = 100)
+        
+        if need_reorder:
+            layer_to_move = self.layers.pop(0)
+            dna_to_move = self.dnaPaths.pop(0)
+            self.layers.append(layer_to_move)
+            self.dnaPaths.append(dna_to_move)
+
+        if need_reorder_eyes:
+            layer_to_move = self.layers.pop(7)
+            dna_to_move = self.dnaPaths.pop(7)
+            self.layers.insert(6, layer_to_move)
+            self.dnaPaths.insert(6, dna_to_move)
 
     def CreateMetadata(self):
         with open(os.path.dirname(__file__) + '/../output/nfts/' + self.folder_path + '/' + str(self.number) + '.json', 'w') as jsonFile:
@@ -55,6 +84,20 @@ class Nft:
             self.metadata['properties']['files'][0] = {"uri" : str(self.number) + ".png", "type":"image/png"}
             json.dump(self.metadata, jsonFile, indent = 4)
 
+    def Check_Reorder(self):
+        if 'Einstein B.png' in self.dnaPaths or 'Viking B.png' in self.dnaPaths or 'Fro B.png' in self.dnaPaths:
+            return True
+        else:
+            return False
 
+    def Reorder_Eyes_Man(self):
+        if 'Glasses Pixel.png' in self.dnaPaths or 'Mohawk Blue.png' in self.dnaPaths or 'Mohawk Red.png' in self.dnaPaths or 'Viking Horns.png' in self.dnaPaths or 'Moonbrain.png' in self.dnaPaths:
+            return True
+        else:
+            return False
 
-
+    def Reorder_Eyes_Female(self):
+        if '3D.png' in self.dnaPaths or 'Glasses Pixel.png' in self.dnaPaths or 'Mohawk Violet.png' in self.dnaPaths or 'Viking Horns.png' in self.dnaPaths or 'Moonbrain.png' in self.dnaPaths:
+            return True
+        else:
+            return False
